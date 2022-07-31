a8757c76074533d8e19194c6cf6d71a9683702f7
Карина <kr_karina00@mail.ru>
2022-07-31 22:54:52 +0300

    Не поняла, какую логику нужно было сделать с кнопкой

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    var counter = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 70
+        redLightView.alpha = 0.3
+        
+        yellowLightView.layer.cornerRadius = 70
+        yellowLightView.alpha = 0.3
+        
+        greenLightView.layer.cornerRadius = 70
+        greenLightView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+        
+           
+        }
+    
+   
+    @IBAction func changeButton() {
+        if counter >= 0 {
+            redLightView.alpha = 1
+            if counter == 2 {
+                yellowLightView.alpha = 1
+                if counter == 3 {
+                    greenLightView.alpha = 1
+                }
+            }
+        }
     }
+    
+    }
+    
+    
+    
 
 
-}
 
